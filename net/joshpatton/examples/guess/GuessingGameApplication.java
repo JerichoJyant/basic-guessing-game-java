490173ec8e8adff2e9139bc283661bcbbd8a8210
Josh Patton <devnamedjosh@gmail.com>
2018-06-07 17:37:46 -0400

    Moving along

--- net/joshpatton/examples/guess/GuessingGameApplication.java
+++ net/joshpatton/examples/guess/GuessingGameApplication.java
@@ -7,18 +7,33 @@ package net.joshpatton.examples.guess;
 import java.util.Scanner;
 
 public class GuessingGameApplication {
-    Scanner scanner;
+    static Scanner scanner;
+    static int playCount = 0;
+    static int winCount = 0;
+
     public static void main(String[] args) {
+        scanner = new Scanner(System.in);
         System.out.println("Guessing Game by Josh Patton");
         System.out.println("Created to demonstrate basic Java skills");
         System.out.println();
-        playOneGame();
+        boolean keepPlaying = true;
+        while(keepPlaying) {
+            playCount++;
+            playOneGame();
+            System.out.print("Press enter to continue, 'n' to quit");
+            if(!scanner.nextLine().equals("")) {
+                quitApplication();
+            }
+        }
     }
 
     public static void playOneGame() {
+        System.out.println("\n*Beginning new game*\n");
         System.out.println("Guess a number 1 - 10");
         System.out.println("Input 'quit' to exit game");
-        Scanner scanner = new Scanner(System.in);
+
+        
+        
         GuessingGameEngine engine = new GuessingGameEngine();
 
         while(!engine.isGameOver()) {
@@ -31,11 +46,12 @@ public class GuessingGameApplication {
                 }
             } catch (NumberFormatException e) {
                 System.out.println("That is not a number. Ending game");
-                userQuitGame();
+                quitApplication();
             }
         }
         if(engine.isGameWon()) {
             System.out.println("Congratulations on winning the game!");
+            winCount++;
         } else {
             System.out.println("You have lost the game.");
             System.out.println("The number was " + engine.getTargetNumber());
@@ -44,8 +60,10 @@ public class GuessingGameApplication {
         
     }
 
-    public static void userQuitGame() {
+    public static void quitApplication() {
         //TODO: show stats
+        System.out.println("\n\n=====");
+        System.out.println("You played " + playCount + " times and won " + winCount + " times");
         System.out.println("Goodbye");
         System.exit(0);
     }
