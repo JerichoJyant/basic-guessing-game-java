41f3a8e710534876be41f753bd360566bf50269a
Josh Patton <devnamedjosh@gmail.com>
2018-06-07 17:50:13 -0400

    Polishing

--- net/joshpatton/examples/guess/GuessingGameApplication.java
+++ net/joshpatton/examples/guess/GuessingGameApplication.java
@@ -20,7 +20,7 @@ public class GuessingGameApplication {
         while(keepPlaying) {
             playCount++;
             playOneGame();
-            System.out.print("Press enter to continue, 'n' to quit");
+            System.out.print("\nPress enter to play again, 'n' to quit: ");
             if(!scanner.nextLine().equals("")) {
                 quitApplication();
             }
@@ -28,25 +28,31 @@ public class GuessingGameApplication {
     }
 
     public static void playOneGame() {
+        // say hi
         System.out.println("\n*Beginning new game*\n");
 
-        System.out.print("What number do you want to guess up to: ");
-
+        // Determine max guess
+        // TODO: This would really be better as a helper function, as it's repeated below;
+        System.out.print("What number do you want to guess up to? ");
         int max = GuessingGameEngine.DEFAULT_MAXIMUM_NUMBER;
         try {
+            String input = scanner.nextLine();
             max = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             System.out.println("That is not a number. Ending game");
             quitApplication();
         }
-
-        System.out.println("Guess a number 1 - 10");
-        System.out.println("Input 'quit' to exit game");
-        
         GuessingGameEngine engine = new GuessingGameEngine(max);
 
+        // intro
+        System.out.println("Guess a number 1 - " + max);
+        System.out.println("Input 'quit' to exit game");
+        
+
+        // Play the game
+
         while(!engine.isGameOver()) {
-            System.out.print("1 - 10: ");
+            System.out.print("1 - " + max + ": ");
             String input = scanner.nextLine().trim();
             try {
                 int guess = Integer.parseInt(input);
@@ -58,6 +64,10 @@ public class GuessingGameApplication {
                 quitApplication();
             }
         }
+
+        // Handle end of game
+        
+        System.out.println("-----");
         if(engine.isGameWon()) {
             System.out.println("Congratulations on winning the game!");
             winCount++;
@@ -66,7 +76,7 @@ public class GuessingGameApplication {
             System.out.println("The number was " + engine.getTargetNumber());
         }
         System.out.println("You made " + engine.getGuessesMade() + " guesses");
-        
+        System.out.println("-----");
     }
 
     public static void quitApplication() {
